92234057cc965b6104a9c03634d06909374d6049
minsun24 <jms49526@gmail.com>
2025-03-31 09:47:26 +0900

    Update settings.py
    fix debug mode true to false

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="change-this-to-a-new-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['minsun24-github-io.onrender.com', 'localhost', '127.0.0.1']
 
